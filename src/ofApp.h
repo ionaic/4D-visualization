f5d73a32807f1d00f0a37a1d8af0403e973f0cee
Ian Ooi <ionaic@gmail.com>
2014-12-09 02:23:46 -0500

    Cube generation, cleaning old index generation

--- src/ofApp.h
+++ src/ofApp.h
@@ -37,39 +37,4 @@ class ofApp : public ofBaseApp{
         
         std::vector<float> hcube_vert;
         std::vector<unsigned int> hcube_idx;
-
-        unsigned int index4D = { 
-             0,  1,  2,  0,  2,  3, // front w0
-             1,  5,  6,  1,  6,  2, // right w0
-             5,  4,  7,  5,  7,  6, // back w0
-             4,  0,  3,  4,  3,  7, // left w0
-             4,  5,  1,  4,  1,  0, // bottom w0
-             3,  2,  6,  3,  6,  7, // top w0
-
-             8,  9, 10,  8, 10, 11, // front w1
-             9, 13, 14,  9, 14, 10, // right w1
-            13, 12, 15, 13, 15, 14, // back w1
-            12,  8, 11, 12, 11, 15, // left w1
-            12, 13,  9, 12,  9,  8, // bottom w1
-            11, 10, 14, 11, 14,  7, // top w1
-
-             8,  9, 10,  8, 10, 11, // front (front w1)
-             9,  1,  2,  9,  2, 10, // right w0/1
-             1,  0,  3,  1,  3,  2, // back (front w0)
-             0,  8, 11,  0, 11,  3, // left w0/1
-             0,  1,  9,  0,  9,  8, // bottom w0/1
-            11, 10,  2, 11,  2,  3, // top w0/1
-
-             1,  9, 10,  1, 10,  2, // front w0/1
-             9, 13, 14,  9, 14, 10, // right (right w1)
-             13, 5,  6,  13, 6, 14, // back w0/1
-             1,  5,  6,  1,  6,  2, // left (right w0)
-             5, 13,  9,  5,  9,  1, // bottom w0/1
-             2, 10, 14,  2, 14,  6, // top w0/1
-
-             5,  4,  7,  5,  7,  6, // front (back w0)
-             5, 13, 14,  5, 14,  6, // right w0/1
-            13, 12, 15, 13, 15, 14, // back (back w1)
-             5, 13, 14,  5, 14,  6, // left w0/1
-        };
 };
